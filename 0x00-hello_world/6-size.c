2a026c2c06bc466c0d75f738d15f175d53e0ecfd
aijukaezragithub <ezraaijuka655@gmail.com>
2022-10-14 02:50:08 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main -Entry
+ *
  * Return: Always 0
  */
 int main(void)
@@ -10,3 +11,4 @@ int main(void)
 	printf("size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("size of a float: %ibyte(s)\n", sizeof(float));
 	return (0);
+}	
